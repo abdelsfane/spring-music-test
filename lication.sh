2ebb6c9b3c8671e361500cdbc743a428273c0fb6
Abdel Fane <abdelsyfane@gmail.com>
2020-01-12 20:48:31 -0600

    upates

--- lication.sh
+++ lication.sh
@@ -1,19 +1,34 @@
 #!/bin/bash
 
 results=""
+GIT_REPO_URL="${GIT_REPO_URL%.*}"
+
+echo "LICATION_ARTIFACT_URL: ${LICATION_ARTIFACT_URL}"
+echo "ART_USERNAME: ${ART_USERNAME}"
+echo "ART_PASSWORD: ${ART_PASSWORD}"
+echo "GIT_REPO_URL: ${GIT_REPO_URL}"
+echo "BUILD_NUMBER: ${BUILD_NUMBER}"
+echo "GIT_TOKEN: ${GIT_TOKEN}"
+echo "LICATION_BACKEND: ${LICATION_BACKEND}"
+echo "CHECKSUM: ${CHECKSUM}"
+
 
 curl -XPOST -H 'Content-type: application/json' -d "{
         \"artifactUrl\": \"${LICATION_ARTIFACT_URL}\",
         \"artifactUser\": \"${ART_USERNAME}\",
         \"artifactPass\": \"${ART_PASSWORD}\",
-        \"githubUrl\": \"${GIT_REPO_URL}\", \"jenkinsJobID\": \"${BUILD_NUMBER}\",
+        \"githubUrl\": \"${GIT_REPO_URL}\",
+        \"jenkinsJobID\": \"${BUILD_NUMBER}\",
         \"githubCreds\": \"${GIT_TOKEN}\"
         }" "${LICATION_BACKEND}"
 
 while [ "$results" = "" ]
 do 
     echo "Checking scan status..."
-    response=`curl -s "${LICATION_BACKEND}"/sha/"${CHECKSUM}" | jq -r '.scanStatus'`
+    response=$(curl -s '${LICATION_BACKEND}/sha/${CHECKSUM}' | jq -r '.scanStatus')
+    echo ${response}
+    echo "response above"
+    results=${response}
 
     if [ "$results" = 2 ]
     then
@@ -36,7 +51,7 @@ do
         echo -e "Scan Completed!\n"
         echo -e "Security Test Failed! Cannot Deploy ${APPLICATION_NAME}!"
         exit 1
-    elif [[ "$results" =~ "null" ]]
+    elif [ "$results" =~ "null" ]
     then
         echo "Return value is null!"
         exit 1
