c0547136993b0cc4960a15330c1375783e9ca3e2
Abdel Fane <abdelsyfane@gmail.com>
2020-01-12 21:00:09 -0600

    milestone1

--- lication.sh
+++ lication.sh
@@ -1,62 +1,64 @@
 #!/bin/bash
 
-results=""
+RESULTS=""
 GIT_REPO_URL="${GIT_REPO_URL%.*}"
-
-echo "LICATION_ARTIFACT_URL: ${LICATION_ARTIFACT_URL}"
+echo "LICATION_ARTIFACT_URL: ${LICATION_ARTIFACT_URL}${APPLICATION_NAME}_${BUILD_NUMBER}.jar"
 echo "ART_USERNAME: ${ART_USERNAME}"
-echo "ART_PASSWORD: ${ART_PASSWORD}"
 echo "GIT_REPO_URL: ${GIT_REPO_URL}"
 echo "BUILD_NUMBER: ${BUILD_NUMBER}"
-echo "GIT_TOKEN: ${GIT_TOKEN}"
 echo "LICATION_BACKEND: ${LICATION_BACKEND}"
 echo "CHECKSUM: ${CHECKSUM}"
+echo "STATUS_ENDPOINT: ${STATUS_ENDPOINT}"
 
-
-curl -XPOST -H 'Content-type: application/json' -d "{
-        \"artifactUrl\": \"${LICATION_ARTIFACT_URL}\",
+    lication_status=`curl -XPOST -H 'Content-type: application/json' -d "{
+        \"artifactUrl\": \"${LICATION_ARTIFACT_URL}${APPLICATION_NAME}_${BUILD_NUMBER}.jar\",
         \"artifactUser\": \"${ART_USERNAME}\",
         \"artifactPass\": \"${ART_PASSWORD}\",
         \"githubUrl\": \"${GIT_REPO_URL}\",
         \"jenkinsJobID\": \"${BUILD_NUMBER}\",
         \"githubCreds\": \"${GIT_TOKEN}\"
-        }" "${LICATION_BACKEND}"
+        }" "${LICATION_BACKEND}"`
 
-while [ "$results" = "" ]
+    echo "${lication_status}"
+
+
+while [ "$RESULTS" = "" ]
 do 
     echo "Checking scan status..."
-    response=$(curl -s '${LICATION_BACKEND}/sha/${CHECKSUM}' | jq -r '.scanStatus')
-    echo ${response}
-    echo "response above"
-    results=${response}
+    RESPONSE=$(curl '${STATUS_ENDPOINT}/sha/${CHECKSUM}' | jq -r '.scanStatus')
+    echo "${RESPONSE}"
+    echo "Results stats above"
+    RESULTS="${RESPONSE}"
 
-    if [ "$results" = 2 ]
+
+    if [ "$RESULTS" = 2 ]
     then
         echo "Scan status is still pending..."
-        results=""
+        RESULTS=""
         sleep ${SLEEP_SECOND}
     
-    elif [ "$results" = 0 ]
+    elif [ "$RESULTS" = 0 ]
     then
         echo -e "Scan completed!\n"
         echo "No vulnerabilities found, deploying ${APPLICATION_NAME}..."
-        cd "${WORKSPACE}"/"$PROJECT_NAME"
+        cd "${WORKSPACE}/$PROJECT_NAME"
         curl -X POST \
             -H 'Content-Type: application/zip' \
             --data-binary @"pcf_artifacts.zip" \
             "${PCF_ENDPOINT}${PCF_ENV}/${PCF_ORG}/${PCF_SPACE}/${APPLICATION_NAME}"
     
-    elif [ "$results" = 1 ]
+    elif [ "$RESULTS" = 1 ]
     then
         echo -e "Scan Completed!\n"
         echo -e "Security Test Failed! Cannot Deploy ${APPLICATION_NAME}!"
         exit 1
-    elif [ "$results" =~ "null" ]
+    elif [[ "$RESULTS" =~ "null" ]]
     then
         echo "Return value is null!"
         exit 1
     else
         echo "Something went wrong! Please review logs"
+        echo "results: ${RESULTS}"
         exit 1
     fi
 done
