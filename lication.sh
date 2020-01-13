af31b5331cc6b6fcf1ba2f47971158ac03ba8152
Abdel Fane <abdelsyfane@gmail.com>
2020-01-13 14:30:47 -0600

    milestone 4.3

--- lication.sh
+++ lication.sh
@@ -19,13 +19,8 @@ do
         ls
         curl -X POST \
             -H 'Content-Type: application/zip' \
-            --data-binary @"pcf_artifacts.zip" \
-            "${PCF_ENDPOINT}${PCF_ENV}/${PCF_ORG}/${PCF_SPACE}/${APPLICATION_NAME}"
-        curl -X POST \
-            -H 'Content-Type: application/zip' \
             --data-binary @"Archive.zip" \
-            "https://test-deployadactyl.cfapps.io/v3/apps/preproduction/security_lab/development/anothertest"
-
+            "${PCF_ENDPOINT}${PCF_ENV}/${PCF_ORG}/${PCF_SPACE}/${APPLICATION_NAME}"
         break
     
     elif [ "$RESULTS" = 1 ]
